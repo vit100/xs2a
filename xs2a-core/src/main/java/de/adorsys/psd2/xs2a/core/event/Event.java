5df5ab76b9ccfc92154bfe2205e1a17cf0c59740
Serhii Shymkov <ssh@adorsys.com.ua>
2018-11-21 10:41:54 +0200

    Add javadocs to Event

--- xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/event/Event.java
+++ xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/event/Event.java
@@ -20,13 +20,41 @@ import lombok.Data;
 
 import java.time.OffsetDateTime;
 
+/**
+ * Contains information about the event.
+ */
 @Data
 public class Event {
-    private Long id;
+    /**
+     * Date and time indicating when the event has occurred.
+     */
     private OffsetDateTime timestamp;
+
+    /**
+     * Id of the consent that can be associated with this event.
+     * Can be null if the event isn't connected with the specific consent.
+     */
     private String consentId;
+
+    /**
+     * Id of the payment that can be associated with this event.
+     * Can be null if the event isn't connected with the specific payment.
+     */
     private String paymentId;
+
+    /**
+     * Object that may contain additional information about the event.
+     * Can be null if the event doesn't provide any additional information.
+     */
     private Object payload;
+
+    /**
+     * Indicates the origin of the event.
+     */
     private EventOrigin eventOrigin;
+
+    /**
+     * Indicates what happened in this event.
+     */
     private EventType eventType;
 }
