4362bb2ec69ef62cb7e684f27f6c3ad63fe69814
Serhii Shymkov <ssh@adorsys.com.ua>
2018-12-28 13:48:16 +0200

    Add description to swagger annotations in CmsAspspPiisController

--- consent-management/consent-aspsp-web/src/main/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspPiisController.java
+++ consent-management/consent-aspsp-web/src/main/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspPiisController.java
@@ -72,6 +72,7 @@ public class CmsAspspPiisController {
         @RequestHeader(value = "psu-corporate-id", required = false) String psuCorporateId,
         @ApiParam(value = "Might be mandated in the ASPSP's documentation. Only used in a corporate context. ")
         @RequestHeader(value = "psu-corporate-id-type", required = false) String psuCorporateIdType,
+        @ApiParam(value = "ID of the particular service instance")
         @RequestHeader(value = "instance-id", required = false, defaultValue = DEFAULT_SERVICE_INSTANCE_ID) String instanceId) {
         PsuIdData psuIdData = getPsuIdData(psuId, psuIdType, psuCorporateId, psuCorporateIdType);
         return new ResponseEntity<>(cmsAspspPiisService.getConsentsForPsu(psuIdData, instanceId), HttpStatus.OK);
@@ -85,6 +86,7 @@ public class CmsAspspPiisController {
     public ResponseEntity<Boolean> terminateConsent(
         @ApiParam(name = "consent-id", value = "The account consent identification assigned to the created account consent.", example = "bf489af6-a2cb-4b75-b71d-d66d58b934d7")
         @PathVariable("consent-id") String consentId,
+        @ApiParam(value = "ID of the particular service instance")
         @RequestHeader(value = "instance-id", required = false, defaultValue = DEFAULT_SERVICE_INSTANCE_ID) String instanceId) {
         return new ResponseEntity<>(cmsAspspPiisService.terminateConsent(consentId, instanceId), HttpStatus.OK);
     }
