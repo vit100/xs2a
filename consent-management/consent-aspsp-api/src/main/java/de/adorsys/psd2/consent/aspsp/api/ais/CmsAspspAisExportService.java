97401dd0bf38baba40673395f3069d389af719bb
Serhii Shymkov <ssh@adorsys.com.ua>
2019-01-02 17:44:11 +0200

    Revert CmsAspspPisExportService

--- consent-management/consent-aspsp-api/src/main/java/de/adorsys/psd2/consent/aspsp/api/ais/CmsAspspAisExportService.java
+++ consent-management/consent-aspsp-api/src/main/java/de/adorsys/psd2/consent/aspsp/api/ais/CmsAspspAisExportService.java
@@ -31,34 +31,32 @@ import java.util.Collection;
 public interface CmsAspspAisExportService {
     /**
      * Returns list of consents by given criteria.
-     *
      * @param tppAuthorisationNumber Mandatory TPP ID
      * @param createDateFrom         Optional starting creation date criteria
      * @param createDateTo           Optional ending creation date criteria
      * @param psuIdData              Optional Psu information criteria
-     * @param instanceId             Mandatory id of particular service instance
-     * @return Collection of consents for TPP by given criteria.
-     * By inconsistent criteria an empty list will be returned
+     *
+     * @return                       Collection of consents for TPP by given criteria.
+     *                               By inconsistent criteria an empty list will be returned
      * @throws TooManyResultsException If CMS is not able to provide result due to overflow,
      *                                 developer shall limit his/her request, making pagination by dates.
      */
     Collection<AisAccountConsent> exportConsentsByTpp(String tppAuthorisationNumber,
                                                       @Nullable LocalDate createDateFrom, @Nullable LocalDate createDateTo,
-                                                      @Nullable PsuIdData psuIdData, @NotNull String instanceId);
+                                                      @Nullable PsuIdData psuIdData);
 
     /**
      * Returns list of consents by given criteria.
      *
-     * @param psuIdData      Mandatory Psu information criteria
-     * @param createDateFrom Optional starting creation date criteria
-     * @param createDateTo   Optional ending creation date criteria
-     * @param instanceId     Mandatory id of particular service instance
-     * @return Collection of consents for TPP by given criteria.
-     * By inconsistent criteria an empty list will be returned
+     * @param psuIdData              Mandatory Psu information criteria
+     * @param createDateFrom         Optional starting creation date criteria
+     * @param createDateTo           Optional ending creation date criteria
+     *
+     * @return                       Collection of consents for TPP by given criteria.
+     *                               By inconsistent criteria an empty list will be returned
      * @throws TooManyResultsException If CMS is not able to provide result due to overflow,
      *                                 developer shall limit his/her request, making pagination by dates.
      */
     Collection<AisAccountConsent> exportConsentsByPsu(PsuIdData psuIdData,
-                                                      @Nullable LocalDate createDateFrom, @Nullable LocalDate createDateTo,
-                                                      @NotNull String instanceId);
+                                                      @Nullable LocalDate createDateFrom, @Nullable LocalDate createDateTo);
 }
