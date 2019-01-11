f493428536f74bdf43dcbba1a116e671479d9f63
may <mpo@adorsys.com.ua>
2019-01-11 17:21:28 +0200

    fixed Account Model Mapper

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/domain/account/AccountModelMapperTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/domain/account/AccountModelMapperTest.java
@@ -295,10 +295,9 @@ public class AccountModelMapperTest {
 
     private Xs2aExchangeRate createExchangeRate() {
         Xs2aExchangeRate exchangeRate = new Xs2aExchangeRate();
-        exchangeRate.setCurrencyFrom(Currency.getInstance("EUR"));
-        exchangeRate.setRateFrom("Rate from");
-        exchangeRate.setCurrencyTo(Currency.getInstance("USD"));
-        exchangeRate.setRateTo("Rate to");
+        exchangeRate.setUnitCurrency("unit currency");
+        exchangeRate.setRate("rate");
+        exchangeRate.setSourceCurrency("source currency");
         exchangeRate.setRateDate(LocalDate.of(2017, 1, 1));
         exchangeRate.setRateContract("Rate contract");
         return exchangeRate;
