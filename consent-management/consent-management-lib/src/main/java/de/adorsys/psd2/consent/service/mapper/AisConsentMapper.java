63fef5bcae8fadb8ea444639d69ce6262159cd9c
Serhii Shymkov <ssh@adorsys.com.ua>
2019-01-28 12:07:21 +0200

    Fix AisAccountAccess mapping in mapToAisAccountConsent
    Use initial AccountAccess in mapToAisAccountConsent if
    AspspAccountAccesses are empty

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/mapper/AisConsentMapper.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/mapper/AisConsentMapper.java
@@ -21,10 +21,10 @@ import de.adorsys.psd2.consent.api.ais.AisAccountAccess;
 import de.adorsys.psd2.consent.api.ais.AisAccountConsent;
 import de.adorsys.psd2.consent.api.ais.AisConsentAuthorizationResponse;
 import de.adorsys.psd2.consent.domain.PsuData;
-import de.adorsys.psd2.consent.domain.account.TppAccountAccess;
 import de.adorsys.psd2.consent.domain.account.AisConsent;
 import de.adorsys.psd2.consent.domain.account.AisConsentAuthorization;
 import de.adorsys.psd2.consent.domain.account.AspspAccountAccess;
+import de.adorsys.psd2.consent.domain.account.TppAccountAccess;
 import de.adorsys.psd2.xs2a.core.profile.AccountReference;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -40,9 +40,16 @@ public class AisConsentMapper {
     private final TppInfoMapper tppInfoMapper;
 
     public AisAccountConsent mapToAisAccountConsent(AisConsent consent) {
+        AisAccountAccess aisAccountAccess;
+        if (consent.getAspspAccountAccesses().isEmpty()) {
+            aisAccountAccess = mapToAisAccountAccess(consent.getAccesses());
+        } else {
+            aisAccountAccess = mapToAspspAisAccountAccess(consent.getAspspAccountAccesses());
+        }
+
         return new AisAccountConsent(
             consent.getExternalId(),
-            mapToAspspAisAccountAccess(consent.getAspspAccountAccesses()),
+            aisAccountAccess,
             consent.isRecurringIndicator(),
             consent.getExpireDate(),
             consent.getTppFrequencyPerDay(),
@@ -91,8 +98,8 @@ public class AisConsentMapper {
 
     private AisAccountAccess mapToAisAccountAccess(List<TppAccountAccess> accountAccesses) {
         return new AisAccountAccess(mapToInitialAccountReferences(accountAccesses, TypeAccess.ACCOUNT),
-            mapToInitialAccountReferences(accountAccesses, TypeAccess.BALANCE),
-            mapToInitialAccountReferences(accountAccesses, TypeAccess.TRANSACTION));
+                                    mapToInitialAccountReferences(accountAccesses, TypeAccess.BALANCE),
+                                    mapToInitialAccountReferences(accountAccesses, TypeAccess.TRANSACTION));
     }
 
     private List<AccountReference> mapToInitialAccountReferences(List<TppAccountAccess> aisAccounts, TypeAccess typeAccess) {
@@ -104,8 +111,8 @@ public class AisConsentMapper {
 
     private AisAccountAccess mapToAspspAisAccountAccess(List<AspspAccountAccess> accountAccesses) {
         return new AisAccountAccess(mapToAccountReferences(accountAccesses, TypeAccess.ACCOUNT),
-            mapToAccountReferences(accountAccesses, TypeAccess.BALANCE),
-            mapToAccountReferences(accountAccesses, TypeAccess.TRANSACTION));
+                                    mapToAccountReferences(accountAccesses, TypeAccess.BALANCE),
+                                    mapToAccountReferences(accountAccesses, TypeAccess.TRANSACTION));
     }
 
     private List<AccountReference> mapToAccountReferences(List<AspspAccountAccess> aisAccounts, TypeAccess typeAccess) {
