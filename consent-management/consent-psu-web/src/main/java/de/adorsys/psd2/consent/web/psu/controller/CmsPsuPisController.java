a70d72a96ec10b5ecbc442e9f594d3472a5b005b
Denys Golubiev <dgo@adorsys.de>
2019-01-30 11:56:34 +0100

    Rename enpoints in CmsPsuPisController to satisfy REST requirements.

--- consent-management/consent-psu-web/src/main/java/de/adorsys/psd2/consent/web/psu/controller/CmsPsuPisController.java
+++ consent-management/consent-psu-web/src/main/java/de/adorsys/psd2/consent/web/psu/controller/CmsPsuPisController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2018 adorsys GmbH & Co KG
+ * Copyright 2018-2019 adorsys GmbH & Co KG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,14 +34,14 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "psu-api/v1/pis/consent")
-@Api(value = "psu-api/v1/pis/consent", tags = "PSU PIS, Consents", description = "Controller for cms-psu-api providing access for PIS consents")
+@RequestMapping(path = "psu-api/v1/payment")
+@Api(value = "psu-api/v1/payment", tags = {"PSU PIS Payment"})
 public class CmsPsuPisController {
     private static final String DEFAULT_SERVICE_INSTANCE_ID = "UNDEFINED";
 
     private final CmsPsuPisService cmsPsuPisService;
 
-    @PutMapping(path = "/redirects/{redirect-id}/psu-data")
+    @PutMapping(path = "/authorisation/{authorisation-id}/psu-data")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK", response = CreatePisCommonPaymentResponse.class),
         @ApiResponse(code = 400, message = "Bad request")})
@@ -92,7 +92,7 @@ public class CmsPsuPisController {
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK", response = CmsPaymentResponse.class),
         @ApiResponse(code = 404, message = "Not Found"),
-        @ApiResponse(code = 408, message = "Request Timeout")})
+        @ApiResponse(code = 408, message = "Request Timeout", response = CmsPaymentResponse.class)})
     public ResponseEntity<CmsPaymentResponse> getPaymentByRedirectId(
         @ApiParam(value = "Client ID of the PSU in the ASPSP client interface. Might be mandated in the ASPSP's documentation. Is not contained if an OAuth2 based authentication was performed in a pre-step or an OAuth2 based SCA was performed in an preceeding AIS service in the same session. ")
         @RequestHeader(value = "psu-id", required = false) String psuId,
@@ -122,7 +122,7 @@ public class CmsPsuPisController {
 
     }
 
-    @GetMapping(path = "/redirects/cancellation/{redirect-id}")
+    @GetMapping(path = "/cancellation/redirects/{redirect-id}")
     @ApiOperation(value = "")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK", response = CmsPaymentResponse.class),
@@ -156,7 +156,7 @@ public class CmsPsuPisController {
         return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
     }
 
-    @PutMapping(path = "/{payment-id}/{authorisation-id}/status/{status}")
+    @PutMapping(path = "/{payment-id}/authorisation/{authorisation-id}/status/{status}")
     @ApiOperation(value = "")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
