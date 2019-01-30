605ac77f9895f6f48e7d96077061b33f76799bf1
Denys Golubiev <dgo@adorsys.de>
2019-01-30 17:28:55 +0100

    Fix `authoriZation`

--- consent-management/consent-psu-web/src/main/java/de/adorsys/psd2/consent/web/psu/controller/CmsPsuAisController.java
+++ consent-management/consent-psu-web/src/main/java/de/adorsys/psd2/consent/web/psu/controller/CmsPsuAisController.java
@@ -92,7 +92,7 @@ public class CmsPsuAisController {
         @PathVariable("consent-id") String consentId,
         @ApiParam(value = "The following code values are permitted 'received', 'psuIdentified', 'psuAuthenticated', 'scaMethodSelected', 'started', 'finalised' 'failed' 'exempted'.", example = "RECEIVED")
         @PathVariable("status") String status,
-        @ApiParam(name = "authorization-id", value = "The consent authorization identification assigned to the created authorization.", example = "bf489af6-a2cb-4b75-b71d-d66d58b934d7")
+        @ApiParam(name = "authorisation-id", value = "The consent authorisation identification assigned to the created authorisation.", example = "bf489af6-a2cb-4b75-b71d-d66d58b934d7")
         @PathVariable("authorisation-id") String authorisationId,
         @ApiParam(value = "Client ID of the PSU in the ASPSP client interface. Might be mandated in the ASPSP's documentation. Is not contained if an OAuth2 based authentication was performed in a pre-step or an OAuth2 based SCA was performed in an preceeding AIS service in the same session. ")
         @RequestHeader(value = "psu-id", required = false) String psuId,
