60d26836294c1eccf97db4ecb7d87475a2797e64
Serhii Shymkov <ssh@adorsys.com.ua>
2019-02-04 17:52:51 +0200

    Export PIIS consents

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/CmsAspspPiisFundsExportServiceInternal.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/CmsAspspPiisFundsExportServiceInternal.java
@@ -17,9 +17,14 @@
 package de.adorsys.psd2.consent.service;
 
 import de.adorsys.psd2.consent.aspsp.api.piis.CmsAspspPiisFundsExportService;
+import de.adorsys.psd2.consent.domain.piis.PiisConsentEntity;
+import de.adorsys.psd2.consent.repository.PiisConsentRepository;
+import de.adorsys.psd2.consent.repository.specification.PiisConsentEntitySpecification;
+import de.adorsys.psd2.consent.service.mapper.PiisConsentMapper;
 import de.adorsys.psd2.xs2a.core.piis.PiisConsent;
 import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
@@ -27,35 +32,49 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CmsAspspPiisFundsExportServiceInternal implements CmsAspspPiisFundsExportService {
+    private final PiisConsentRepository piisConsentRepository;
+    private final PiisConsentEntitySpecification piisConsentEntitySpecification;
+    private final PiisConsentMapper piisConsentMapper;
+
     @Override
     public Collection<PiisConsent> exportConsentsByTpp(String tppAuthorisationNumber,
                                                        @Nullable LocalDate createDateFrom,
                                                        @Nullable LocalDate createDateTo, @Nullable PsuIdData psuIdData,
-                                                       @NotNull String instanceId
-                                                      ) {
-        // TODO implement method https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/613
-        return Collections.emptyList();
+                                                       @NotNull String instanceId) {
+        if (StringUtils.isBlank(tppAuthorisationNumber) || StringUtils.isBlank(instanceId)) {
+            return Collections.emptyList();
+        }
+
+        List<PiisConsentEntity> piisConsentEntities = piisConsentRepository.findAll(piisConsentEntitySpecification.byTppIdAndCreationPeriodAndPsuIdDataAndInstanceId(tppAuthorisationNumber, createDateFrom, createDateTo, psuIdData, instanceId));
+        return piisConsentMapper.mapToPiisConsentList(piisConsentEntities);
     }
 
     @Override
     public Collection<PiisConsent> exportConsentsByPsu(PsuIdData psuIdData, @Nullable LocalDate createDateFrom,
-                                                       @Nullable LocalDate createDateTo, @NotNull String instanceId
-                                                      ) {
-        // TODO implement method https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/613
-        return Collections.emptyList();
+                                                       @Nullable LocalDate createDateTo, @NotNull String instanceId) {
+        if (psuIdData == null || psuIdData.isEmpty() || StringUtils.isBlank(instanceId)) {
+            return Collections.emptyList();
+        }
+
+        List<PiisConsentEntity> piisConsentEntities = piisConsentRepository.findAll(piisConsentEntitySpecification.byPsuIdDataAndCreationPeriodAndInstanceId(psuIdData, createDateFrom, createDateTo, instanceId));
+        return piisConsentMapper.mapToPiisConsentList(piisConsentEntities);
     }
 
     @Override
     public Collection<PiisConsent> exportConsentsByAccountId(@NotNull String aspspAccountId,
                                                              @Nullable LocalDate createDateFrom,
                                                              @Nullable LocalDate createDateTo,
-                                                             @NotNull String instanceId
-                                                            ) {
-        // TODO implement method https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/613
-        return Collections.emptyList();
+                                                             @NotNull String instanceId) {
+        if (StringUtils.isBlank(aspspAccountId) || StringUtils.isBlank(instanceId)) {
+            return Collections.emptyList();
+        }
+
+        List<PiisConsentEntity> piisConsentEntities = piisConsentRepository.findAll(piisConsentEntitySpecification.byAspspAccountIdAndCreationPeriodAndInstanceId(aspspAccountId, createDateFrom, createDateTo, instanceId));
+        return piisConsentMapper.mapToPiisConsentList(piisConsentEntities);
     }
 }
