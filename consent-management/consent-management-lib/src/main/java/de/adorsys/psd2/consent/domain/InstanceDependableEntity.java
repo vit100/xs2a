f112a9de1b9a4d61593d40bf3ba82462ca299c38
Serhii Shymkov <ssh@adorsys.com.ua>
2019-02-13 15:17:49 +0200

    Add TODO to create integration tests

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/domain/InstanceDependableEntity.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/domain/InstanceDependableEntity.java
@@ -26,6 +26,7 @@ import javax.persistence.MappedSuperclass;
 @Setter
 @MappedSuperclass
 public abstract class InstanceDependableEntity {
+    // TODO Create integration tests in CMS to verify whether instanceId is set https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/690
     private static final String DEFAULT_SERVICE_INSTANCE_ID = "UNDEFINED";
 
     @Column(name = "instance_id", nullable = false, updatable = false)
