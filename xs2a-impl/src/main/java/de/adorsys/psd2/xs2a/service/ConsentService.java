30e1c053c1929b47895183b1585cbc35bff3598d
Mykola Shumyn <msh@adorsys.com.ua>
2019-02-18 16:43:11 +0200

    - fixed MessageErrorCode for ais endpoint forbidden case.

--- xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/ConsentService.java
+++ xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/ConsentService.java
@@ -284,7 +284,7 @@ public class ConsentService {
 
         if (!endpointAccessCheckerService.isEndpointAccessible(updatePsuData.getAuthorizationId(), updatePsuData.getConsentId())) {
             return ResponseObject.<UpdateConsentPsuDataResponse>builder()
-                .fail(new MessageError(ErrorType.AIS_403, new TppMessageInformation(MessageCategory.ERROR, MessageErrorCode.FORBIDDEN)))
+                .fail(new MessageError(ErrorType.AIS_403, new TppMessageInformation(MessageCategory.ERROR, MessageErrorCode.SERVICE_BLOCKED)))
                 .build();
         }
 
