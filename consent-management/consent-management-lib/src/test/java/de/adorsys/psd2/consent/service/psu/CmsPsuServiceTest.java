9d19dd7c86fddbe86d89f6a8807ba5ab7cf810c2
Serhii Shymkov <ssh@adorsys.com.ua>
2019-02-20 18:39:51 +0200

    Add tests for CmsPsuService#isPsuDataListEqual

--- consent-management/consent-management-lib/src/test/java/de/adorsys/psd2/consent/service/psu/CmsPsuServiceTest.java
+++ consent-management/consent-management-lib/src/test/java/de/adorsys/psd2/consent/service/psu/CmsPsuServiceTest.java
@@ -24,6 +24,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,9 +34,13 @@ public class CmsPsuServiceTest {
     private static final String PSU_ID_1 = "psu id 1";
     private static final String PSU_ID_2 = "psu id 2";
     private static final String PSU_ID_3 = "psu id 3";
-    private final PsuData PSU_DATA_1 = new PsuData(PSU_ID_1, "psu type", "corp id", "corp type");
-    private final PsuData PSU_DATA_2 = new PsuData(PSU_ID_2, "psu type", "corp id", "corp type");
-    private final PsuData PSU_DATA_3 = new PsuData(PSU_ID_3, "psu type", "corp id", "corp type");
+    private static final String PSU_ID_TYPE = "psu type";
+    private static final String PSU_CORPORATE_ID = "corp id";
+    private static final String PSU_CORPORATE_ID_TYPE = "corp type";
+
+    private final PsuData PSU_DATA_1 = new PsuData(PSU_ID_1, PSU_ID_TYPE, PSU_CORPORATE_ID, PSU_CORPORATE_ID_TYPE);
+    private final PsuData PSU_DATA_2 = new PsuData(PSU_ID_2, PSU_ID_TYPE, PSU_CORPORATE_ID, PSU_CORPORATE_ID_TYPE);
+    private final PsuData PSU_DATA_3 = new PsuData(PSU_ID_3, PSU_ID_TYPE, PSU_CORPORATE_ID, PSU_CORPORATE_ID_TYPE);
     private final List<PsuData> PSU_DATA_LIST = new ArrayList<>(Arrays.asList(PSU_DATA_1, PSU_DATA_2));
 
     @InjectMocks
@@ -90,5 +95,52 @@ public class CmsPsuServiceTest {
         boolean actualResult = cmsPsuService.isPsuDataNew(PSU_DATA_3, PSU_DATA_LIST);
 
         assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void isPsuDataListEqual_shouldReturnTrue_sameList() {
+        boolean actualResult = cmsPsuService.isPsuDataListEqual(PSU_DATA_LIST, PSU_DATA_LIST);
+
+        assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void isPsuDataListEqual_shouldReturnTrue_sameContentExceptId() {
+        List<PsuData> psuDataList = Arrays.asList(buildPsuDataWithId(PSU_ID_1, 1L), buildPsuDataWithId(PSU_ID_2, 2L));
+        List<PsuData> anotherPsuDataList = Arrays.asList(buildPsuDataWithId(PSU_ID_1, 3L), buildPsuDataWithId(PSU_ID_2, 4L));
+
+        boolean actualResult = cmsPsuService.isPsuDataListEqual(psuDataList, anotherPsuDataList);
+
+        assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void isPsuDataListEqual_shouldReturnTrue_emptyLists() {
+        boolean actualResult = cmsPsuService.isPsuDataListEqual(Collections.emptyList(), Collections.emptyList());
+
+        assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void isPsuDataListEqual_shouldReturnFalse_differentPsuIds() {
+        List<PsuData> psuDataList = Arrays.asList(PSU_DATA_1, PSU_DATA_2);
+        List<PsuData> anotherPsuDataList = Arrays.asList(PSU_DATA_2, PSU_DATA_3);
+
+        boolean actualResult = cmsPsuService.isPsuDataListEqual(psuDataList, anotherPsuDataList);
+
+        assertThat(actualResult).isFalse();
+    }
+
+    @Test
+    public void isPsuDataListEqual_shouldReturnFalse_oneEmptyList() {
+        boolean actualResult = cmsPsuService.isPsuDataListEqual(PSU_DATA_LIST, Collections.emptyList());
+
+        assertThat(actualResult).isFalse();
+    }
+
+    private PsuData buildPsuDataWithId(String psuId, Long databaseId) {
+        PsuData newPsuData = new PsuData(psuId, PSU_ID_TYPE, PSU_CORPORATE_ID, PSU_CORPORATE_ID_TYPE);
+        newPsuData.setId(databaseId);
+        return newPsuData;
     }
 }
